43b32ee0a25bc58c9da3d0907983c9979ce723c7
Lokendra Bairwa <lokendra@talkshopclub.com>
2024-11-12 16:32:30 +0530

    Remove Terraform configuration files

--- terraform/main.tf
+++
@@ -1,9 +0,0 @@
-# Create an ECR repository
-resource "aws_ecr_repository" "itsm_bot_backend" {
-  name                 = "itsm-bot/backend"
-  image_tag_mutability = "MUTABLE"
-
-  encryption_configuration {
-    encryption_type = "AES256"
-  }
-}
